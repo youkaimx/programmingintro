5f0be2c7ce40743faa700f48d30840a4b044ef67
youkaimx <rvaldez@gmail.com>
2019-09-19 18:59:54 -0500

    Actualización previa a clase

---
+++ source/fibonacci_v2.py
@@ -0,0 +1,18 @@
+# Imprime n elementos de la serie de fibonacci
+
+entrada = input("Cuantos elementos? ")
+elementos = int(entrada)
+n1 = 1
+n2 = 1
+if elementos >= 1:
+    print(n1,end=" ")
+if elementos >= 2:
+    print(n2,end=" ")
+if elementos >= 3:
+    contador = 2
+    while contador < elementos:
+        siguiente = n1 + n2
+        print(siguiente,end=" ")
+        n1 = n2
+        n2 = siguiente
+        contador = contador + 1
